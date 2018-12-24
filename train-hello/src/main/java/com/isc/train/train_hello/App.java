f1bb64cb92cc1a3a0f75adfffe50e412fb13bf9e
Theerapat <t.komeinek@gmail.com>
2018-12-24 12:36:42 +0700

    Cleaning

--- train-hello/src/main/java/com/isc/train/train_hello/App.java
+++ train-hello/src/main/java/com/isc/train/train_hello/App.java
@@ -4,10 +4,8 @@ package com.isc.train.train_hello;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main( String[] args ){
+		System.out.println( "Hello World!" );
+	}
 }
