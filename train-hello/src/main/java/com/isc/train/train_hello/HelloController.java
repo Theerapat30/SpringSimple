0885023e031faea2f5696fd6cea59aaa5a6a2b89
Theerapat <t.komeinek@gmail.com>
2018-12-24 22:41:12 +0700

    Added simple json

--- train-hello/src/main/java/com/isc/train/train_hello/HelloController.java
+++ train-hello/src/main/java/com/isc/train/train_hello/HelloController.java
@@ -1,14 +1,23 @@
 package com.isc.train.train_hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	String home() {
 		return "Hello World!";
+	}
+	
+	@RequestMapping("/greeting")
+	public GreetingBean greeting(@RequestParam(value="greeting", defaultValue="Greet") String name) {
+		GreetingBean bean = new GreetingBean();
+		bean.setRespCode("9000");
+		bean.setRespDesc("Status OK");
+		return bean;
 	}
 
 }
